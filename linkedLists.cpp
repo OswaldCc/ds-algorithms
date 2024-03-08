c0dfcd5e664e975d3a5ef62d5fe1942d3b1d0f8e
OswaldCc <sam.kabati@gmail.com>
2024-03-08 12:29:44 +0300

    clarifications that were necessary

--- linkedLists.cpp
+++ linkedLists.cpp
@@ -4,26 +4,20 @@ using namespace std;
 class Node{
 public:
 	int data;
-	Node* next;	// this means next is a pointer of type node
+	Node* next;	
 	
-	//default conctructor(creates new Node objects with default values)
+
 	Node(){
 		data=0;
 		next=NULL;
 	} 
-	//parametized constructor(this one is used when you want to create a node object with a specific data value provided
 	  Node(int data) 
     { 
         this->data = data; 
         this->next = NULL; 
     } 
 };
-/* the role of this class is to manage the overall structure of the linked list such as insertion and deletion.
-it does not need to handle the individual data elements directly.by
-not dealing stores within each node, the ll class becomes more generic and flexible,
-it can be used to mange linked lists containing any type of data, not just a specific type.
 
-*/
 class LinkedList{
 	Node* head;
 	public:
@@ -46,23 +40,13 @@ void LinkedList::insertNode(int data){
 		head=newNode;
 		return;
 	}
-	Node* temp =head;// this initializes pointer temp of type Node and sets it to the head pointer, to traverse the node from the beginning
+	Node* temp =head;// we want to start looping from here+-
 	
-	//traverse till the end of the list
-	/*this loop works because if you for example instatiate a new instance of linked 
-	list and add more than just the head, it means that that instance has for example two
-	instances of class node. hence if there is a next pointer in the same instance of list we 
-	will find it(the pointer is automatically created when you instantiate a new instance of class node). this will also
-	make the last item in the linked list have a null pointer because no new instance of node has been created
-    hence we are able to traverse through that one instance of linkedlist. the temp varable will point to the last node.remember 
-	that the last node is characterised by its pointer being null. 
-	*/
-	while(temp->next != NULL){//we are referring to the next variable of the next node instance
+
+	while(temp->next != NULL){//despite temp by itself just being a pointer, we have to explicitly say, temp->next cause if we just do temp we will also be referring to the data attribute of the temp variable 
 		temp= temp->next;//The loop's goal is to find the last node in the linked list. The last node is characterized by having its next pointer set to NULL
 		
 	}
-	// we update the next pointer to point to the next node of class Node
-	//here we are just updating the last pointer but remember that when new node is created the data variable already exists
 	temp->next=newNode;
 	
 }
@@ -72,7 +56,7 @@ void LinkedList::printList(){
 		cout<<"List is empty"<<endl;
 		return;
 	}
-	while(temp!=NULL){
+	while(temp!=NULL){//if we looped using temp->next,the last number would not be printed out because it has a NULL pointer,using temp refers to the memory address as well as the data attribute and they would both have to be null for printing to stop.
 		cout<<temp->data<<" ";
 		temp=temp->next;//check out this line next time 
 	}
